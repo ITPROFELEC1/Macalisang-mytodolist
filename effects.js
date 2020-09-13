dd88da5df34b7b5686aed8f45072888ce8e54746
chevil123 <chevil.macalisang29@gmail.com>
2020-09-13 15:23:20 +0800

    update

---
+++ effects.js
@@ -0,0 +1,26 @@
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+  ev.target.classList.toggle('checked');
+  }
+}, false);
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+  alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+  }
+function removeAll(){
+  var lst = document.getElementsByTagName("ul");
+    lst[0].innerHTML = "";
+}
+function removeLine(){ 
+var lst1= document.getElementById("myUL");
+lst1.removeChild(list.childNodes[0]);
+}
